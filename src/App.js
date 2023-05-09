f02264a9321730d6f77661bd5f0d090f251b70aa
anacris16 <titialtuve1605@gmail.com>
2023-05-09 19:51:32 +0200

    added the GitHub repository url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">App </div>;
+  return (
+    <div className="App">
+      App
+      <footer>
+        This project was coded by Ana Cristina Altuve and is {""}
+        <a
+          href="https://github.com/anacris16/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
