35f3624ae19f4e8ab3d9fcf4318b86b8471ebf61
anacris16 <titialtuve1605@gmail.com>
2023-05-09 20:32:17 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      App
-      <footer>
-        This project was coded by Ana Cristina Altuve and is {""}
-        <a
-          href="https://github.com/anacris16/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        App
+        <footer>
+          This project was coded by Ana Cristina Altuve and is {""}
+          <a
+            href="https://github.com/anacris16/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
