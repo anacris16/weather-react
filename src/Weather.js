3b22936bcbcc14d206f2ab09462c0a368b5ef4df
anacris16 <titialtuve1605@gmail.com>
2023-05-10 20:18:04 +0200

    updated weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       description: response.data.condition.description,
       feels_like: response.data.temperature.feels_like,
-      icon: response.data.condition.icon_url,
+      icon: response.data.condition.icon,
       date: new Date(response.data.time * 1000),
     });
   }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -12,15 +13,16 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
-          <img
-            src={props.data.icon}
-            alt={props.data.Description}
-          />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">ºC</span>
+        <div className="col-6 d-flex">
+          <div className="float-left">
+            <WeatherIcon code={props.data.icon} />
+          </div>
+          <div className="float-left">
+            <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="unit">ºC</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
