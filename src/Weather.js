53f19652195a0076b32c7349105d56c03f99e54a
anacris16 <titialtuve1605@gmail.com>
2023-05-22 15:02:05 +0200

    added loading spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast"
+import WeatherForecast from "./WeatherForecast";
 import "./Weather.css";
+import { SpinnerRoundFilled } from "spinners-react";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
-  
+
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: response.data.temperature.current,
@@ -26,7 +26,10 @@ export default function Weather(props) {
   function search() {
     const apiKey = "0b2bf100cdt092834da45do9a6a6377e";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    axios
+      .get(apiUrl)
+      .then(handleResponse)
+      .catch((err) => console.log(err));
   }
 
   function handleSubmit(event) {
@@ -62,11 +65,15 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast city={weatherData.city}/>
+        <WeatherForecast city={weatherData.city} />
       </div>
     );
   } else {
     search();
-    return "Loading...";
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <SpinnerRoundFilled color="#430f58" size={100} />
+      </div>
+    );
   }
 }
